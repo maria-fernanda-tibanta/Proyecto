98af0f5a6a93775e6d78d9092d57f83fd123db91
DESKTOP-EFTFVFD\david <david-hernandezbgu@hotmail.com>
2016-11-20 22:27:12 -0500

    completando area y perimetro de figuras geometricas

---
+++ FIGURAS GEOMETRICAS/Figuras Geometricas (2).py
@@ -0,0 +1,87 @@
+import math
+print("DAVID HERNANDEZ- MARIA FERNANDA TIBANTA")
+print("Perimetro y area de las figuras geometricas")
+print ("\n")
+n=int(input("Ingrese el numero de lados de una figura geometrica: "))
+
+def triangulo():
+    print ("El numero de lados pertenece a un triangulo")
+    print ("\n")
+    print ("Perimetro del triangulo")
+    l=int(input("Ingrese el valor del lado del triangulo: "))
+    perimetro=l*3
+    print ("Perimetro igual a: ", perimetro)
+    print ("\n")
+    print ("Area del triangulo")
+    b=int(input("Ingrese la base del triangulo: "))
+    h=int(input("Ingrese la altura del triangulo: "))
+    area=(b*h)/2
+    print ("Area igual a: ", area)
+
+def cuadrado():
+    print ("El numero de lados pertenece a un cuadrado")
+    print ("\n")
+    print ("Perimetro del cuadrado")
+    l=int(input("Ingrese el valor del lado del cuadrado: "))
+    perimetro=l*4
+    print ("Perimetro igual a: ", perimetro)
+    print ("\n")
+    print ("Area del cuadrado")
+    area=l**2
+    print ("Area igual a: ", area)
+
+def pentagono():
+    print ("El numero de lados pertenece a un pentagono")
+    print ("\n")
+    print ("Perimetro del pentagono")
+    l=int(input("Ingrese el valor del lado del pentagono: "))
+    perimetro=l*5
+    print ("Perimetro igual a: ", perimetro)
+    print ("\n")
+    print ("Area del pentagono")
+    ap=int(input("Ingrese el valor del apotema del pentagono: "))
+    area=(5*l*ap)/2
+    print ("Area igual a: ", area)
+
+
+def hexagono():
+    print("Su figura es un hexagono")
+    lado=int(input("Ingresa la longitud del lado del hexagono: "))
+    apotema= math.sqrt((lado**2)-(lado/2)**2)
+    ph=lado*6
+    print("El perimetro del hexagono es: ",ph)
+    ah= (6*lado*apotema)/2
+    print("El area del hexagono es: ",round(ah,2))
+def heptagono():
+    print("su figura es un pentagono")
+    lado=int(input("Ingresa la longitud del lado del heptagono: "))
+    ppe= 7*lado
+    apotema=math.sqrt((lado**2)-(lado/2)**2)
+    print("El perimetro del hentagono es: ",ppe)
+    ape=(7*lado*apotema)/2
+    print("El area del heptagono es: ",round(ape,2))
+def octogono ():
+    print("su figura es un octogono")
+    lado=int(input("Ingresa la longitud del lado del octogono: "))
+    poc=8*lado
+    apotema=math.sqrt((lado**2)-(lado/2)**2)
+    print("El perimetro del octogono es: ",poc)
+    aoc=(4*lado*apotema)
+    print("El area del octogono es: ",round(aoc,2))
+
+def figuras ():
+    if (n==3):
+        triangulo()
+    elif (n==4):
+        cuadrado()
+    elif (n==5):
+        pentagono()
+    elif n==6:
+        hexagono()
+    elif n==7:
+        heptagono()
+    elif n==8:
+        octogono()
+
+
+figuras()
