f06dffb5403b9a3bc0854fa9b01b7b79023adc18
DESKTOP-EFTFVFD\david <david-hernandezbgu@hotmail.com>
2016-11-21 18:39:00 -0500

    ultimas modificaciones figuras geometricas

--- FIGURAS GEOMETRICAS/Figuras Geometricas.py
+++ FIGURAS GEOMETRICAS/Figuras Geometricas.py
@@ -1,3 +1,5 @@
+import math
+print("DAVID HERNANDEZ- MARIA FERNANDA TIBANTA")
 print("Perimetro y area de las figuras geometricas")
 print ("\n")
 n=int(input("Ingrese el numero de lados de una figura geometrica: "))
@@ -41,7 +43,40 @@ def pentagono():
     area=(5*l*ap)/2
     print ("Area igual a: ", area)
     
+def hexagono():
+    print("Su figura es un hexagono")
+    lado=int(input("Ingresa la longitud del lado del hexagono: "))
+    apotema= math.sqrt((lado**2)-(lado/2)**2)
+    ph=lado*6
+    print("El perimetro del hexagono es: ",ph)
+    ah= (6*lado*apotema)/2
+    print("El area del hexagono es: ",round(ah,2))
+def heptagono():
+    print("su figura es un pentagono")
+    lado=int(input("Ingresa la longitud del lado del heptagono: "))
+    ppe= 7*lado
+    apotema=math.sqrt((lado**2)-(lado/2)**2)
+    print("El perimetro del hentagono es: ",ppe)
+    ape=(7*lado*apotema)/2
+    print("El area del heptagono es: ",round(ape,2))
+def octogono ():
+    print("su figura es un octogono")
+    lado=int(input("Ingresa la longitud del lado del octogono: "))
+    poc=8*lado
+    apotema=math.sqrt((lado**2)-(lado/2)**2)
+    print("El perimetro del octogono es: ",poc)
+    aoc=(4*lado*apotema)
+    print("El area del octogono es: ",round(aoc,2))
 
+def eneagono():
+     print("su figura es un eneagono")
+     lado=int(input("Ingresa la longitud del lado del eneagono: "))
+     pen=9*lado
+     apotema=math.sqrt((lado**2)-(lado/2)**2)
+     print("El perimetro del eneagono es: ",pen)
+     aen=(pen*apotema)/2
+     print("El area del eneagono es: ",round(aen,2))
+     
 def figuras ():
     if (n==3):
         triangulo()
@@ -49,6 +84,14 @@ def figuras ():
         cuadrado()
     elif (n==5):
         pentagono()
+    elif n==6:
+        hexagono()
+    elif n==7:
+        heptagono()
+    elif n==8:
+        octogono()
+    elif n==9:
+        eneagono()
         
 
 
